809911c6db7dc3d312808896936724ba43d82a78
Chawer Kamanga <chawerkamanga@gmail.com>
2022-06-06 11:13:27 +0200

    added organization and other dashboard mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGray: 'hsl(257, 7%, 63%)',
+        lightGray: 'hsl(0, 0%, 96%)',
+        textLightGray: 'hsl(0, 0%, 51%)',
+        progressBraLightGray: 'hsl(0, 0%, 85%)',
+        darkBlue: 'hsl(218, 37%, 44%)',
+      },
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
