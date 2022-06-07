695705a87ee7652a10cddc112ff106bcf76433d3
Chawer Kamanga <chawerkamanga@gmail.com>
2022-06-07 04:17:24 +0200

    added tablet side navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
+    screens: {
+      md: '768px',
+      lg: '900px',
+      xl: '1240px',
+    },
     extend: {
       colors: {
         darkGray: 'hsl(257, 7%, 63%)',
@@ -8,6 +13,7 @@ module.exports = {
         textLightGray: 'hsl(0, 0%, 51%)',
         progressBraLightGray: 'hsl(0, 0%, 85%)',
         darkBlue: 'hsl(218, 37%, 44%)',
+        veryDarkBlue: 'hsl(218, 43%, 23%)',
       },
       fontFamily: {
         sans: ['Manrope', 'sans-serif'],
