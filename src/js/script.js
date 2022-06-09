57dc898565ee32125f4d25b2e2f4ed0a1dca4b4c
Chawer Kamanga <chawerkamanga@gmail.com>
2022-06-09 13:10:40 +0200

    added circle pregress bar with divs

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
+const bigPercentage = document.getElementById('big-percentage');
+
 btn.addEventListener("click", navToggle);
 
 function navToggle() {
@@ -8,3 +10,15 @@ function navToggle() {
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 }
+
+const maxValue = parseInt(bigPercentage.innerText);
+
+let counter = 0;
+setInterval(() => {
+  if(counter == maxValue ){
+    clearInterval();
+  }else{
+    counter+=1;
+    bigPercentage.textContent = counter;
+  }
+}, 60);
